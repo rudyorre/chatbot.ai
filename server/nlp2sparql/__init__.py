8d78174f0fe724c9a1eddb3f95098ebfc745c872
rudyorre <rudyorre@gmail.com>
2023-02-07 01:04:55 -0800

    Add stemming functionality to predicate matching
    - Stemmer keeps predicate matching consistent bc sometimes words have slightly different forms
    - Ex: stem('imports') == 'import', making user queries more flexible

--- server/nlp2sparql/__init__.py
+++ server/nlp2sparql/__init__.py
@@ -1,8 +1,9 @@
 from SPARQLWrapper import SPARQLWrapper, JSON
 
 import nltk
-from nltk.tokenize import word_tokenize
+from nltk.tokenize import word_tokenize, sent_tokenize
 from nltk.corpus import stopwords
+from nltk.stem import PorterStemmer
 
 import re
 
@@ -15,6 +16,7 @@ class FusekiClient:
         self._sparql = SPARQLWrapper(connection_string)
         self._sparql.setReturnFormat(JSON)
         self._predicate_cache = dict()
+        self._query_cache = dict()
 
     def parse(self, text: str) -> list:
         '''
@@ -53,22 +55,35 @@ class FusekiClient:
         Returns:
             ...
         '''
+        # Identifying subject and predicate
         subject = tagged_tokens[1][0] # subject: analysis, base, bundle, mission, project, etc
         predicate = tagged_tokens[2][0] # predicate: imports, description, etc
+
+        # Stemming initialization
+        ps = PorterStemmer()
+        predicate_stem = ps.stem(predicate)
+
+        # Checking if previous query was made already
+        if (subject, predicate) in self._query_cache:
+            return self._query_cache[(subject, predicate)]
+        
         result = self.sparql_query(subject=f'foundation:{subject}')
         filtered_result = list()
         for i in range(len(result)):
             # Split string with delimiters ('/' and '#')
             predicate_uri = result[i]['predicate']['value']
             predicate_name = re.split(r'/|#', predicate_uri)[-1]
-            if predicate_name == predicate:
+            predicate_name_stem = ps.stem(predicate_name)
+            if predicate_name == predicate or predicate_stem == predicate_name_stem:
                 self._predicate_cache[predicate_name] = predicate_uri
                 filtered_result.append(result[i])
+
         return filtered_result
 
     def sparql_query(self, subject='?subject', predicate='?predicate', object='?object'):
         '''
-        Description.
+        Creates a sparql query using the provided subject, predicate, and object variables. By default,
+        these variables will be unknown.
 
         Args:
             subject:
@@ -76,15 +91,16 @@ class FusekiClient:
             object:
         
         Returns:
+            A list of dictionaries containing representing the JSON that the SPARQL query returns.
         '''
-        self._sparql.setQuery(f"""
+        self._sparql.setQuery(f'''
             PREFIX foundation:<http://imce.jpl.nasa.gov/foundation/>
 
             SELECT ?subject ?predicate ?object
             WHERE {{
                 {subject} {predicate} {object}
             }}
-            """
+            '''
         )
         result = []
         try:		
@@ -94,4 +110,3 @@ class FusekiClient:
         except Exception as e:
             print(e)
         return result
-
