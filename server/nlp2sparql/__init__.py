4eb0be100e9bc089a2bfdfb77a07c528782a7c2a
rudyorre <rudyorre@gmail.com>
2023-02-07 00:12:57 -0800

    Made the `what_query()` method generalize
    - Any query within the foundation of firesat can be done
    - Still limited to queries of type f(subject, predicate) -> object

--- server/nlp2sparql/__init__.py
+++ server/nlp2sparql/__init__.py
@@ -4,6 +4,8 @@ import nltk
 from nltk.tokenize import word_tokenize
 from nltk.corpus import stopwords
 
+import re
+
 nltk.download('stopwords')
 nltk.download('punkt')
 nltk.download('averaged_perceptron_tagger')
@@ -12,9 +14,10 @@ class FusekiClient:
     def __init__(self, connection_string: str):
         self._sparql = SPARQLWrapper(connection_string)
         self._sparql.setReturnFormat(JSON)
+        self._predicate_cache = dict()
 
     def parse(self, text: str) -> list:
-        """
+        '''
         Tokenizes and tags a string of words, while also filtering out stop words
         to reduce the sentence down to its most minimal form.
 
@@ -25,7 +28,7 @@ class FusekiClient:
         Returns:
             A list of 2-tuples representing the filtered and tokenized sentence. Format: ("word", "parts-of-speech tag")
                 Ex: [('What', 'WP'), ('mission', 'NN'), ('description', 'NN'), ('?', '.')]
-        """
+        '''
         # Tokenize user query
         tokens = nltk.word_tokenize(text)
         # Filter redundant words
@@ -36,7 +39,7 @@ class FusekiClient:
         return tagged_tokens
 
     def what_query(self, tagged_tokens: list):
-        """
+        '''
         Processes user queries that ask a basic "what" question. In the context of RDF triples, the user
         should prompt a subject and predicate, which will match all objects that match the criteria. The
         implementation will involve making a more general query initially where only the subject is included,
@@ -49,19 +52,37 @@ class FusekiClient:
         
         Returns:
             ...
-        """
+        '''
         subject = tagged_tokens[1][0] # subject: analysis, base, bundle, mission, project, etc
         predicate = tagged_tokens[2][0] # predicate: imports, description, etc
+        result = self.sparql_query(subject=f'foundation:{subject}')
+        filtered_result = list()
+        for i in range(len(result)):
+            # Split string with delimiters ('/' and '#')
+            predicate_uri = result[i]['predicate']['value']
+            predicate_name = re.split(r'/|#', predicate_uri)[-1]
+            if predicate_name == predicate:
+                self._predicate_cache[predicate_name] = predicate_uri
+                filtered_result.append(result[i])
+        return filtered_result
+
+    def sparql_query(self, subject='?subject', predicate='?predicate', object='?object'):
+        '''
+        Description.
+
+        Args:
+            subject:
+            predicate:
+            object:
         
+        Returns:
+        '''
         self._sparql.setQuery(f"""
             PREFIX foundation:<http://imce.jpl.nasa.gov/foundation/>
-            
-            PREFIX imports:<http://www.w3.org/2002/07/owl#imports>
-            PREFIX description:<http://purl.org/dc/elements/1.1/description>
 
             SELECT ?subject ?predicate ?object
             WHERE {{
-                foundation:{subject} {predicate}: ?object
+                {subject} {predicate} {object}
             }}
             """
         )
