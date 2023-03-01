be88535535e433a58b522e8eb9124af93c3d8558
Yuwen Jia <michelle2010tx@hotmail.com>
2023-03-01 12:36:36 -0800

    Merge with query pattern

--- server/src/server/fuseki/__init__.py
+++ server/src/server/fuseki/__init__.py
@@ -89,3 +89,53 @@ class FusekiClient:
         except Exception as e:
             print(e)
         return result
+
+    def assembly_query(
+        self,
+        assembly="?assembly",
+        id="?id",
+        mass="?mass",
+        function="?function",
+    ):
+        """
+        Creates a sparql query using the provided assembly object, id, mass and function variables. By default,
+        these variables will be unknown.
+
+        Args:
+            assembly: an object of fse:Assembly
+            id: id of assembly
+            mass: mass of assembly
+            function: function of assembly
+        
+        Returns:
+            A list of dictionaries containing representing the JSON that the SPARQL query returns.
+        """
+
+        self._sparql.setQuery(
+            f"""
+            PREFIX fse:			<http://opencaesar.io/examples/firesat/disciplines/fse/fse#>
+            PREFIX base:		<http://imce.jpl.nasa.gov/foundation/base#>
+            PREFIX analysis: 	<http://imce.jpl.nasa.gov/foundation/analysis#>
+            PREFIX vim4: 		<http://bipm.org/jcgm/vim4#>
+            PREFIX mission: 	<http://imce.jpl.nasa.gov/foundation/mission#>
+            
+            SELECT DISTINCT ?assembly ?id ?mass ?function
+            WHERE {{
+                {assembly} a fse:Assembly ;									# match an assembly
+                        base:hasIdentifier "{id}" ;							# match its id
+                        analysis:isCharacterizedBy [						# match its mass
+                            vim4:hasDoubleNumber {mass}
+                        ] ;
+                        mission:performs {function} .						# match a function it performs
+            }}
+            """
+        )
+
+        result = []
+        try:		
+            ret = self._sparql.queryAndConvert()	
+            for r in ret["results"]["bindings"]:
+                result.append(r)
+        except Exception as e:
+            print(e)
+        return result
