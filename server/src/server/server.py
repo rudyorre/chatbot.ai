9a87b957c7a358b13ed979db969dfba014a2d928
Samuel Shen <sam.shen321@gmail.com>
2023-03-09 18:49:15 -0800

    added example queries to frontpage

--- server/src/server/server.py
+++ server/src/server/server.py
@@ -55,26 +55,21 @@ processed_queries = []
 @app.route("/query", methods=["POST"])
 @cross_origin()
 def user_query():
-	# querystr = request.get_json()["data"]
-	# user_query = Query(querystr)
-	# user_queries.append(user_query)
-
-	# transformed_query = parse_user_query(querystr)
-	# resp = get_hardcode_response(transformed_query)
-	# resp = {"response": resp}
-	# if resp["response"] == "":
-	# 	print("empty hardcode response")
-	# 	processed_query = nlqe.query(user_query)
-	# 	processed_queries.append(processed_query)
-	# else:
-	# 	processed_queries.append(resp)
-	# return "", 204
-	user_query = Query(request.get_json()["data"])
+	querystr = request.get_json()["data"]
+	user_query = Query(querystr)
 	user_queries.append(user_query)
-	processed_query = nlqe.query(user_query)
-	processed_queries.append(processed_query)
-	return "", 204
 
+	transformed_query = parse_user_query(querystr)
+	resp = get_hardcode_response(transformed_query)
+	resp = {"response": resp}
+	
+	if resp["response"] == "":
+		#empty hardcode response, query nlqe
+		processed_query = nlqe.query(user_query)
+		processed_queries.append(processed_query)
+	else:
+		processed_queries.append(resp)
+	return "", 204
 
 @app.route("/response")
 @cross_origin()
